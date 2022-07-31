adc2267ea0f79a184028ee894a98fe10702b8acd
gardenee <snnjjang@naver.com>
2022-07-31 10:57:37 +0900

    220731

--- lunchwb/save_cate.py
+++ lunchwb/save_cate.py
@@ -1,3 +1,32 @@
+import collections
+import cx_Oracle
+
+def insert_1st(cate_name):
+    conn = cx_Oracle.connect("lunchwb", "lunchwb", "localhost:1521/xe")
+    cs = conn.cursor()
+
+    sql = "insert into food_1st_category values (seq_food_1st_category_no.nextval, :cate_1st_name)"
+
+    cs.execute(sql, cate_1st_name=cate_name)
+    conn.commit()
+
+    cs.close()
+    conn.close()
+
+
+def insert_2nd(cate_1st_no, cate_2nd):
+    conn = cx_Oracle.connect("lunchwb", "lunchwb", "localhost:1521/xe")
+    cs = conn.cursor()
+
+    sql = "insert into food_2nd_category values (seq_food_2nd_category_no.nextval, :cate_1st_no, :cate_2nd)"
+
+    cs.execute(sql, cate_1st_no=cate_1st_no, cate_2nd=cate_2nd)
+    conn.commit()
+
+    cs.close()
+    conn.close()
+
+
 category_dict = collections.OrderedDict()
 category_dict['뷔페'] = ['해산물뷔페', '한식뷔페', '고기뷔페', '뷔페']
 category_dict['아시아음식'] = ['터키음식', '동남아음식', '인도음식', '아시아음식']
@@ -10,6 +39,12 @@ category_dict['치킨'] = ['치킨']
 category_dict['분식'] = ['분식']
 category_dict['중식'] = ['중식']
 
-conn = cx_Oracle.connect("lunchwb", "lunchwb", "localhost:1521/xe")
 
+cate_1st = category_dict.keys()
+
+for idx, cate in enumerate(list(category_dict.keys())):
+    insert_1st(cate)
+
+    for cate_2 in category_dict[cate]:
+        insert_2nd(idx+1, cate_2)
 
